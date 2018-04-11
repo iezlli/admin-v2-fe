12bb9f73cac063fbf0e05edad285e60b998a020e
lizhuliang <2922520570@qq.com>
2018-04-11 21:33:54 +0800

    1st

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+// // console.log(123)
+
+// let a=12;
+// let test=(value)=>{
+//     return value*2;
+// };
+
+// test(1);
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './index.css';
+import './index.scss';
+
+ReactDom.render(
+        <h1>hello react webpack </h1>,
+        document.getElementById('app')
+    );
